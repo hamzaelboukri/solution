c03196c690bc64106f9a2fcd2ba33f13dd3fdf7a
hamzaelboukri <hamzaelboukri01@gmail.com>
2025-09-22 16:37:31 +0100

    add

---
+++ backend/Server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app   = express();
+app.use(express.static (path.join(__dirname, '../frontend/index.html')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
