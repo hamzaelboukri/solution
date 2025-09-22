db886c5f0231feabffd4301dfab255b7ad4c1db0
hamzaelboukri <hamzaelboukri01@gmail.com>
2025-09-22 19:27:35 +0100

    add a database

--- backend/Server.js
+++ backend/Server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const path = require('path');
-const app   = express();
-app.use(express.static (path.join(__dirname, '../frontend/index.html')));
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/Login.html'));
+});
+
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/Signup.html'));
 });
 
 const PORT = 3000;
---
+++ backend/database.js
@@ -0,0 +1,17 @@
+const myslq = require('mysql');
+
+const db = myslq.createConnection({
+    host: 'localhost',
+    user: 'root',       
+    password: '',
+    database: 'fin_solutions'
+});
+
+db.connect((err) => {   
+    if (err) {
+        console.error('Erreur de connexion à la base de données:', err);
+    }
+    else {
+        console.log('Connecté à la base de données MySQL.');
+    }    
+});
